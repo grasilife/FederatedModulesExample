1f9c44b7229b09483c9a640e45a0a1c8d91898f1
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-10-27 23:11:58 +0800

    创建应用基本文件

---
+++ packages/application-b/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+const mode = process.env.NODE_ENV || 'production';
+
+module.exports = {
+  mode,
+  entry: './src/index',
+  devtool: 'source-map',
+  optimization: {
+    minimize: mode === 'production',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')],
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
